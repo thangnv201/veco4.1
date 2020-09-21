5ed1a1c4fcd28a6e8d23aa1223a046db4187fcc9
nhatnh4 <Hongnhat97tt@gmail.com>
2020-09-21 16:23:46 +0700

    commit ki ver2

--- plugins/kpi_report/app/controllers/cnbv_kpi_controller.rb
+++ plugins/kpi_report/app/controllers/cnbv_kpi_controller.rb
@@ -2,6 +2,7 @@ class CnbvKpiController < ApplicationController
   include MyKpiHelper
 
   $kidanhgia = Project.find(1072).versions.first.id
+
   def index
     if params.key?("kidanhgia")
       $kidanhgia = params["kidanhgia"].to_i
@@ -14,9 +15,25 @@ class CnbvKpiController < ApplicationController
     @kpi_raking.each do |kpi|
       users_id.push(kpi.user_id)
     end
-    @kpi_raking1 = PeopleKi.where(user_id: users_id, version_id:$kidanhgia)
-    sql = "select * from (select * from users   WHERE  users.id in ("+users_id.join(",")+"))a left join people_kis on a.id=people_kis.user_id AND `people_kis`.`version_id` = "+$kidanhgia.to_s+" order by a.id"
+    @kpi_raking1 = PeopleKi.where(user_id: users_id, version_id: $kidanhgia)
+    sql = "select * from (select * from users   WHERE  users.id in (" + users_id.join(",") + "))a left join people_kis on a.id=people_kis.user_id AND `people_kis`.`version_id` = " + $kidanhgia.to_s + " order by a.id"
     @records_array = ActiveRecord::Base.connection.execute(sql)
+    @ki_raking = @records_array.as_json
+
+  end
+
+  def test
+
+    if params[:ki_id] == ""
+      uid = PeopleInformation.where(employee_id: params[:user_code]).take.user_id
+      PeopleKi.create(:user_id => uid, :version_id => params[:version_id], :location_compliance => params[:location],
+                      :labor_rules_compliance => params[:labor_rules], :ki => params[:ki],
+                      :manager_note => params[:manage_note], :note => params[:note]);
+    else
+      PeopleKi.update(params[:ki_id], :location_compliance => params[:location],
+                      :labor_rules_compliance => params[:labor_rules], :ki => params[:ki],
+                      :manager_note => params[:manage_note], :note => params[:note]);
+    end
 
   end
 end
