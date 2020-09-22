c89ac5b6cdef97b88d8d2af187fa0c8af520e60b
nhatnh4 <Hongnhat97tt@gmail.com>
2020-09-22 17:29:03 +0700

    commit ki ver5

--- plugins/kpi_report/app/controllers/cnbv_kpi_controller.rb
+++ plugins/kpi_report/app/controllers/cnbv_kpi_controller.rb
@@ -9,30 +9,35 @@ class CnbvKpiController < ApplicationController
     else
       $kidanhgia = Project.find(1072).versions.first.id
     end
+    # $pmid = User.current.id
     $pmid = 860
     @kpi_raking = PeopleInformation.where(manager_id: $pmid).order(:user_id)
     users_id = []
     @kpi_raking.each do |kpi|
       users_id.push(kpi.user_id)
     end
-    @kpi_raking1 = PeopleKi.where(user_id: users_id, version_id: $kidanhgia)
-    sql = "select * from (select * from users   WHERE  users.id in (" + users_id.join(",") + "))a left join people_kis on a.id=people_kis.user_id AND `people_kis`.`version_id` = " + $kidanhgia.to_s + " order by a.id"
-    @records_array = ActiveRecord::Base.connection.execute(sql)
-    @ki_raking = @records_array.as_json
+    if users_id.size == 0
+      @ki_raking = nil
+    else
+      sql = "select * from (select * from users   WHERE  users.id in (" + users_id.join(",") + "))a left join people_kis on a.id=people_kis.user_id AND `people_kis`.`version_id` = " + $kidanhgia.to_s + " order by a.id"
+      @records_array = ActiveRecord::Base.connection.execute(sql)
+      @ki_raking = @records_array.as_json
+    end
+
   end
 
   def save
-
-    if params[:ki_id] == ""
       uid = PeopleInformation.where(employee_id: params[:user_code]).take.user_id
-      PeopleKi.create(:user_id => uid, :version_id => params[:version_id], :location_compliance => params[:location],
-                      :labor_rules_compliance => params[:labor_rules], :ki => params[:ki],
-                      :manager_note => params[:manage_note], :note => params[:note]);
-    else
-      PeopleKi.update(params[:ki_id], :location_compliance => params[:location],
-                      :labor_rules_compliance => params[:labor_rules], :ki => params[:ki],
-                      :manager_note => params[:manage_note], :note => params[:note]);
-    end
-
+      check_create = PeopleKi.where(user_id: uid, version_id: params[:version_id]).size
+      if check_create > 0
+        pkid=PeopleKi.where(user_id: uid, version_id: params[:version_id]).first.id
+        PeopleKi.update(pkid, :location_compliance => params[:location],
+                        :labor_rules_compliance => params[:labor_rules], :ki => params[:ki],
+                        :manager_note => params[:manage_note], :note => params[:note]);
+      else
+        PeopleKi.create(:user_id => uid, :version_id => params[:version_id], :location_compliance => params[:location],
+                        :labor_rules_compliance => params[:labor_rules], :ki => params[:ki],
+                        :manager_note => params[:manage_note], :note => params[:note]);
+      end
   end
 end
